377ee64282b666339a32d4021ef3378ccb49b8bd
mvpnitesh <parimalanitesh@gmail.com>
2021-04-08 09:47:11 +0530

    Updated Linked list program

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -28,7 +28,56 @@ class LinkedList(object):
 			return
 		node.next = previous_node.next
 		previous_node.next = node
+	
+	def delete_key(self, position):
+		temp = self.head
+		if position==0:
+			self.head=self.head.next
+			return
+		for i in range(position-1):
+			temp = temp.next
+			if temp is None:
+				break
+		if temp is None or temp.next is None:
+			print("Position is greater than the length of the linked list")
+			return
+		temp.next = temp.next.next	
 		
+	def delete_linked_list(self):
+		self.head = None
+	
+	def length_of_linked_list(self):
+		length = 0
+		temp = self.head
+		while (temp):
+			length+=1
+			temp = temp.next
+		return length
+	
+	def search(self, data):
+		temp = self.head
+		is_present = False
+		while(temp):
+			if temp.data == data:
+				is_present = True
+				break
+			else:
+				temp = temp.next
+		return is_present
+
+	def nth_node(self,n):
+		temp = self.head
+		if n == 0:
+			return self.head
+		start = 0
+		while (temp):
+			if start == n:
+				return temp.data
+			else:
+				temp = temp.next
+				start+=1
+		if start < n:
+			print('n th position is greater than length of the list')
 
 new_linked_list = LinkedList()
 a = Node(00)
@@ -43,8 +92,16 @@ new_linked_list.push_at_begining(Node(40))
 new_linked_list.push_at_ending(Node(50))
 new_linked_list.append_after(a,Node(10))
 #new_linked_list.printlist()
-
+head = new_linked_list.head
+new_linked_list.delete_key(5)
+#new_linked_list.delete_linked_list()
 head = new_linked_list.head
 while (head):
 	print (head.data)
 	head = head.next
+len = new_linked_list.length_of_linked_list()
+print('Length of linked list is ', len)
+is_present = new_linked_list.search(100)
+print('Element present is ', is_present)
+n_th_node = new_linked_list.nth_node(10)
+print('Data in the N th node is ',n_th_node)
--- interview_questions/occurance_in_array.py
+++ interview_questions/occurance_in_array.py
@@ -5,12 +5,15 @@ most_occurance = 0
 for i in array:
     if i in occurance_dict.keys():
         occurance_dict[i] = occurance_dict[i] + 1
-        if occurance_dict[i] > most_occurance:
-            most_occurance = occurance_dict[i]
-            most_occuranced_number  = i
     else:
         occurance_dict[i] = 1
 
-# for i in occurance_dict.keys():
-   
+for i in occurance_dict.keys():
+    if occurance_dict[i] > most_occurance:
+		most_occurance = occurance_dict[i]
+		most_occuranced_number  = i
 print (most_occuranced_number)
+
+
+
+0.08sec 89.024
